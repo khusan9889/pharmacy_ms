2342051052e72168f888df01701e7fa8e7b0e0dd
khusan9889 <k.xusan_2003@mail.ru>
2023-02-21 12:19:26 +0500

    Email validation done. it can be only unique and checks from db

--- src/users/services/users/users.service.ts
+++ src/users/services/users/users.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, InternalServerErrorException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository, FindOneOptions } from 'typeorm';
 import { User } from 'src/typeorm';
@@ -8,23 +8,38 @@ export class UserService {
   constructor(
     @InjectRepository(User)
     private readonly userRepository: Repository<User>,
-  ) {}
+  ) { }
 
   async findAll(): Promise<User[]> {
     return this.userRepository.find();
   }
 
   async findOne(id: number): Promise<User> {
-    const options: FindOneOptions<User> = { where: {id} }
+    const options: FindOneOptions<User> = { where: { id } }
     return this.userRepository.findOne(options);
   }
 
+  // async create(user: User): Promise<User> {
+  //   return this.userRepository.save(user);
+  // }
+
   async create(user: User): Promise<User> {
-    return this.userRepository.save(user);
+    const existingUser = await this.userRepository.findOne({ where: { email: user.email } });
+    if (existingUser) {
+      throw new BadRequestException('This email is already in use. Please try with another email');
+      // return existingUser;
+    }
+
+    // try {
+    await this.userRepository.save(user);
+    return user;
+    // } catch (error) {
+    //   throw new InternalServerErrorException('Failed to create user');
+    // }
   }
 
   async update(id: number, user: User): Promise<User> {
-    const options: FindOneOptions<User> = { where: {id} }
+    const options: FindOneOptions<User> = { where: { id } }
     await this.userRepository.update(id, user);
     return this.userRepository.findOne(options);
   }
