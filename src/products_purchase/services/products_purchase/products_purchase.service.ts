80e280172bfe5d2c6366d4dd99642d89238d9239
khusan9889 <k.xusan_2003@mail.ru>
2023-03-03 12:53:05 +0500

    when a purchase is created while products_purchase is made it records in the purchase table counting the total_price and the id of purchase is indicated in products_purchase

--- src/products_purchase/services/products_purchase/products_purchase.service.ts
+++ src/products_purchase/services/products_purchase/products_purchase.service.ts
@@ -19,10 +19,9 @@ export class ProductsPurchaseService {
   async create(product_purchases: any[]): Promise<ProductPurchase[]> {
     const productPurchaseRecords = [];
     let total_price = 0;
-
     const purchase = new Purchase();
     purchase.productPurchase = [];
-
+  
     for (const product_purchase of product_purchases) {
       const product = await this.productsService.findOne(product_purchase.product);
       await this.productsService.purchaseProduct(product_purchase?.product, product_purchase.amount);
@@ -32,23 +31,29 @@ export class ProductsPurchaseService {
         price,
       };
       productPurchaseRecords.push(productPurchase);
-
+  
       const purchaseItem = new ProductPurchase();
+      // Set the purchase ID for the product purchase item
       purchaseItem.purchase = purchase;
       purchaseItem.product = { id: product_purchase.product } as any;
       purchaseItem.amount = product_purchase.amount;
       purchaseItem.price = price;
       purchase.productPurchase.push(purchaseItem);
-
+  
       total_price += price * product_purchase.amount;
     }
-
+  
     purchase.total_price = total_price;
-
-    // Save the Purchase and its associated ProductPurchase entities
-    await this.purchaseRepository.save(purchase);
-
+    // Save the purchase to get the ID
+    const savedPurchase = await this.purchaseRepository.save(purchase);
+    // Set the purchase ID for each product purchase item
+    purchase.productPurchase.forEach((item) => {
+        item.purchase = savedPurchase;
+    });
+    // Save the product purchase items with the updated purchase IDs
+    await this.productPurchaseRepository.save(purchase.productPurchase);
+  
     return productPurchaseRecords;
-  }
+}
 }
 
