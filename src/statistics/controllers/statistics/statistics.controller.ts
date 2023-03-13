ed85b7ef16b97ae779e160f61c17e4a0c37e8cd9
khusan9889 <k.xusan_2003@mail.ru>
2023-03-14 00:56:22 +0500

    resul.dto added to users statistics

--- src/statistics/controllers/statistics/statistics.controller.ts
+++ src/statistics/controllers/statistics/statistics.controller.ts
@@ -1,18 +1,35 @@
 import { Controller, Get, Param } from '@nestjs/common';
 import { StatisticsService } from 'src/statistics/services/statistics/statistics.service';
+import { ResultDto } from 'dto/result.dto';
+
+
 
 @Controller('statistics')
 export class StatisticsController {
   constructor(private readonly statisticsService: StatisticsService) {}
 
   @Get()
-  async getUserPurchaseStats(): Promise<{ userId: number; numPurchases: number; totalPrice: number }[]> {
-    return this.statisticsService.getUserPurchaseStats();
+  async getUserPurchaseStats(): Promise<ResultDto<{ userId: number; numPurchases: number; totalPrice: number }[]>> {
+    try {
+      const stats = await this.statisticsService.getUserPurchaseStats();
+      return new ResultDto(true, null, stats);
+    } catch (error) {
+      return new ResultDto(false, null, null, error);
+    }
   }
 
   @Get(':userId')
-  async getUserPurchaseStatsByUserId(@Param('userId') userId: string): Promise<{ userId: number; numPurchases: number; totalPrice: number }> {
-    const stats = await this.statisticsService.getUserPurchaseStats();
-    return stats.find(stat => stat.userId === parseInt(userId));
+  async getUserPurchaseStatsByUserId(@Param('userId') userId: string): Promise<ResultDto<{ userId: number; numPurchases: number; totalPrice: number }>> {
+    try {
+      const stats = await this.statisticsService.getUserPurchaseStats();
+      const userStats = stats.find(stat => stat.userId === parseInt(userId));
+      if (userStats) {
+        return new ResultDto(true, null, userStats);
+      } else {
+        return new ResultDto(false, `User with ID ${userId} not found`);
+      }
+    } catch (error) {
+      return new ResultDto(false, null, null, error);
+    }
   }
 }
