afbd6c17192ba78007daba29606c19f71e803908
khusan9889 <k.xusan_2003@mail.ru>
2023-04-07 17:33:58 +0500

    test server

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
+ 
   // Add cookie parser middleware
   app.use(cookieParser());
 
